0f4aaed3b6beaa86cbce68fc7871984f83264369
veetenergy <Malxasyanvahag634@gmail.com>
2025-02-27 10:49:56 +0300

    папку users  поменял по 2 уроку

--- lessons/leson_1/test-db.sql
+++ lessons/leson_1/test-db.sql
@@ -1,1 +1,11 @@
-SELECT id, name FROM persons;
+--Этот код создаст в базе данных таблицу 
+CREATE TABLE persons(
+    id INT,
+    name VARCHAR(255)
+);
+
+--Этот код добавит в таблицу 2 записи
+INSERT INTO persons (id,name)
+VALUES (0,'Tomas'),(1,'Martin');
+
+SELECT * FROM persons
